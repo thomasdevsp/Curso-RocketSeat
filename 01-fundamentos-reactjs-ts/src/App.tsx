0e946cb91f89502a2845b77d4c0baf36a07a109f
thomasdevsp <thomasdevsp@gmail.com>
2023-12-09 11:41:47 -0300

    Projeto Do Ignite

---
+++ 01-fundamentos-reactjs-ts/src/App.tsx
@@ -0,0 +1,63 @@
+import { Post, PostType } from './components/Post'
+import { Header } from './components/Header'
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
+
+import './global.css';
+
+
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/79715330?v=4',
+      name: 'Thomas Fernando',
+      role: 'Aluno @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-16 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/6643122?v=4',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-07-16 20:00:00'),
+  },
+];
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                post={post}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://avatars.githubusercontent.com/u/79715330?v=4" />
+
+        <strong>Thomas Fernando</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
