a68d701087df6578a0414deed9514dc750512cc2
thomasdevsp <thomasdevsp@gmail.com>
2023-12-12 20:10:35 -0300

    Add new archive Context and correct bugs

--- 02-ignite-timer/src/pages/History/index.tsx
+++ 02-ignite-timer/src/pages/History/index.tsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './styles'
+import { CyclesContext } from '../../contexts/CyclesContext'
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
