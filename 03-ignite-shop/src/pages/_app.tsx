9c8741b7eecabcbe64dd2612971215fbc62fc8e0
thomasdevsp <thomasdevsp@gmail.com>
2023-12-18 13:32:01 -0300

    Adding a logo

--- 03-ignite-shop/src/pages/_app.tsx
+++ 03-ignite-shop/src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { globalStyles } from '../styles/global'
 import logoImg from '../assets/logo.svg'
 import { Container, Header } from '../styles/pages/app'
 
+import Image from 'next/image'
+
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
 
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
