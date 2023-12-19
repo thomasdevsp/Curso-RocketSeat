089f640a454311cca6830c7d67ce9db5db4a2c96
thomasdevsp <thomasdevsp@gmail.com>
2023-12-19 14:27:22 -0300

    Commit

--- 03-ignite-shop/src/pages/index.tsx
+++ 03-ignite-shop/src/pages/index.tsx
@@ -1,14 +1,27 @@
 import Image from "next/image";
+
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import camiseta1 from '../assets/Shirt/1.png'
 import camiseta2 from '../assets/Shirt/2.png'
 import camiseta3 from '../assets/Shirt/3.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -17,11 +30,29 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta XY</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta x</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
