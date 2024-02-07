82cf813a95f326c70766e22c029d4a1dd07fab50
thomasdevsp <thomasdevsp@gmail.com>
2024-02-07 16:17:33 -0300

    Criando e Estilizando o Componente: Summary

--- 04-dt-money/src/pages/Transactions/index.tsx
+++ 04-dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,29 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
 
 export function Transaction() {
   return (
     <div>
       <Header />
+      <Summary />
+
+      <table>
+        <tbody>
+          <tr>
+            <td width="50%">Desenvolvimento de site</td>
+            <td>R$ 12.000,00</td>
+            <td>Venda</td>
+            <td>13/04/2023</td>
+          </tr>
+
+          <tr>
+            <td width="50%">Hamburger</td>
+            <td>- R$ 59,00</td>
+            <td>Alimentação</td>
+            <td>10/04/2023</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
