717645f3552e9d84c510bb5d87a0ac8e6ddd99a9
thomasdevsp <thomasdevsp@gmail.com>
2024-02-07 09:55:29 -0300

    Ending a project

--- 03-ignite-shop/src/pages/success.tsx
+++ 03-ignite-shop/src/pages/success.tsx
@@ -4,6 +4,7 @@ import { stripe } from "../lib/stripe";
 import { GetServerSideProps } from "next";
 import Stripe from "stripe";
 import Image from "next/image";
+import Head from "next/head";
 
 interface SuccessProps {
   customerName: string;
@@ -15,25 +16,43 @@ interface SuccessProps {
 
 export default function Success({ customerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra Efetuada!</h1>
+    <>
+      <Head>
+        <title>Compra Efetuada | Ignite Shop</title>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt="" />
-      </ImageContainer>
+        <meta name="robots" content="noindex" />
+      </Head>
 
-      <p>
-        Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
 
-      <Link href="/">
-        Voltar ao catálago
-      </Link>
-    </SuccessContainer>
+      <SuccessContainer>
+        <h1>Compra Efetuada!</h1>
+
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
+
+        <p>
+          Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
+
+        <Link href="/">
+          Voltar ao catálago
+        </Link>
+      </SuccessContainer>
+    </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
   const sessionId = String(query.session_id);
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
