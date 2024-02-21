bb817397865b075fb40d2881e10acca162061a50
thomasdevsp <thomasdevsp@gmail.com>
2024-02-21 16:48:10 -0300

    Creating a serch form

--- 04-dt-money/src/pages/Transactions/index.tsx
+++ 04-dt-money/src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
   return (
@@ -7,23 +9,35 @@ export function Transaction() {
       <Header />
       <Summary />
 
-      <table>
-        <tbody>
-          <tr>
-            <td width="50%">Desenvolvimento de site</td>
-            <td>R$ 12.000,00</td>
-            <td>Venda</td>
-            <td>13/04/2023</td>
-          </tr>
+      <TransactionsContainer>
+        <SearchForm />
 
-          <tr>
-            <td width="50%">Hamburger</td>
-            <td>- R$ 59,00</td>
-            <td>Alimentação</td>
-            <td>10/04/2023</td>
-          </tr>
-        </tbody>
-      </table>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 59,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
